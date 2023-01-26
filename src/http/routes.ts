22f2218e959bdce979c9a4f770dff69de345743c
James Spence <jamesaspence20@gmail.com>
2023-01-26 12:55:24 -0600

    feat: middleware and routing config

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,5 +1,9 @@
 import { version } from './../../package.json';
 import Router from '@koa/router';
+import { validate } from './middleware/validate';
+import { jwtAuth } from './middleware/jwt';
+import { login, me, registerUser } from './controller/auth';
+import { loginValidator, registrationValidator } from './request/auth';
 
 const router = new Router();
 
@@ -11,6 +15,14 @@ router.get('/', ctx => {
 });
 
 const apiRouter = new Router({ prefix: '/api/v1' });
+
+apiRouter.post(
+  '/auth/register',
+  validate(registrationValidator()),
+  registerUser
+);
+apiRouter.post('/auth/login', validate(loginValidator()), login);
+apiRouter.get('/me', jwtAuth, me);
 
 router.use(apiRouter.routes());
 router.use(apiRouter.allowedMethods());
